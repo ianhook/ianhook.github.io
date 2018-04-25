de51d2052fc6d3dff4415ae8105d46911f0914b1
Ian Hook <ianhook@gmail.com>
2018-04-24 21:53:21 -0700

    add Description

---
+++ src/Description.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Description = styled.p`
+    text-indent: -18px;
+    padding-left: 36px;
+`;
+
+export default Description;
